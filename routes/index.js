aa13334c7ac4f510e6fd62e77b50e55d4e084a50
Gladsonms <gladsonsaji4@gmail.com>
2021-09-06 21:05:46 +0530

    BASIC LOGIN

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const username="gladson"
+const password="12345"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   res.render('index');
 });  
+router.post('/login',(req,res)=> {
+
+if(username==req.body.username && password==req.body.password)
+{
+  console.log("succes");
+  res.redirect('/home')
+}
+else
+
+{
+  res.redirect('/login')
+}
+})
 
 module.exports = router;
